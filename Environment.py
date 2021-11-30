07fd498f6256b7544ad7c351f638f341c55fa1ed
Ramtin Nouri <Ramtin.Nouri@outlook.de>
2021-11-30 19:50:37 +0100

    minor bugfix in envwrapper

--- Environment.py
+++ Environment.py
@@ -3,19 +3,20 @@ import os,random,cv2,numpy as np
 import gym
 
 # preprocessing used by Karpathy (cf. https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5)
-def preprocess_frame_karpathy(I):
+def preprocess_frame_karpathy(I,is2D):
   """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
   I = I[35:195] # crop
   I = I[::2,::2,0] # downsample by factor of 2
   I[I == 144] = 0 # erase background (background type 1)
   I[I == 109] = 0 # erase background (background type 2)
   I[I != 0] = 1 # everything else (paddles, ball) just set to 1
+  if is2D: return np.expand_dims(I.astype(np.float),axis=2)
   return I.astype(np.float).reshape(80*80) #TODO: make dependent of stack
 
 
 class EnvWrapper():
     """
-        Wrapper around actual OpenAi Gym environent with some processing of the observations
+        Wrapper around actual OpenAi Gym environent with some processing of the observations. Specifically for Atari Pong Environment.
     """
     def __init__(self,env_id,use_preprocessing,use_diff=True,stack=False):
         """
@@ -96,7 +97,7 @@ class EnvWrapper():
         """
         observation, reward, done, info = self.env.step(action)
         if self.use_preprocessing:
-            observation = preprocess_frame_karpathy(observation)
+            observation = preprocess_frame_karpathy(observation,self.stack)
         else:
             observation = np.array(observation)/255
         if not self.use_diff:
