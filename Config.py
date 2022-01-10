6259b6aa3d5e4061235ce249c6f4f2f72871ad15
Ramtin Nouri <Ramtin.nouri>
2022-01-11 00:12:58 +0100

    config for M1 (experiment 0)

--- Config.py
+++ Config.py
@@ -3,27 +3,27 @@ Collection of all global variables
 """
 
 # Number of individuals
-NUM_AGENTS = 60
+NUM_AGENTS = 200
 # Number of total generations
-EPISODES = 100
+EPISODES = 200
 # OpenAi Gym Environment
 ENV = "Pong-v4"
 # Use Custom Environment Wrapper
 WRAP_ENV = True
 # Number of individuals to keep unchanged
-ELITE_SIZE = 10
+ELITE_SIZE = 20
 # Number of completely new individuals introduced
-ALIENS = 5
+ALIENS = 10
 # Number of maximal simulation steps after which to stop
-MAX_SIM_STEPS = 500
+MAX_SIM_STEPS = 2000
 # Constant to add whenever we divide where the variable could be 0
 EPSILON = 1e-8
 # Mutation Factors:
 MUTATION_FACTOR_SUBSTITUTE = 0.1
-MUTATION_FACTOR_INSERT_DELETE = 0.1
-MUTATION_FACTOR_DUPLICATE = 0.1
-MUTATION_FACTOR_INVERT = 0.1
+MUTATION_FACTOR_INSERT_DELETE = 0
+MUTATION_FACTOR_DUPLICATE = 0
+MUTATION_FACTOR_INVERT = 0
 # Type of Model: choice between Dense and CNN
 MODELTYPE = "Dense"
 # Whether to have asexual reproduction or include crossover
-IS_ASEXUAL = False
+IS_ASEXUAL = True
